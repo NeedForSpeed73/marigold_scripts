646820b8c9c517b16c3641e56e8505e519aaa57a
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-09 19:55:56 +0100

    Update 1_Clone_MarigoldRepo.sh

--- 1_Clone_MarigoldRepo.sh
+++ 1_Clone_MarigoldRepo.sh
@@ -2,6 +2,6 @@
 #
 
 # Clone Marigold repo
-printf %"s\n" "" "* Cloning Marigold Repository ""
+printf %"s\n" "" "* Cloning Marigold Repository "
 git clone https://github.com/prs-eth/Marigold.git
 cd Marigold
