0de485b1740f51f042b99252d584da465db2be52
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-09 19:43:06 +0100

    Update 1_Install_Miniforge.sh

--- 1_Install_Miniforge.sh
+++ 1_Install_Miniforge.sh
@@ -6,13 +6,10 @@ printf %"s\n" "" "* Downloading Miniforge3-"$(uname)-$(uname -m) ""
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 
 printf %"s\n" "" "* Installing Miniforge3-"$(uname)-$(uname -m) ""
-bash "Miniforge3-"$(uname)-$(uname -m)".sh"  -b -p "${HOME}/miniforge3"
+bash "Miniforge3-"$(uname)-$(uname -m)".sh"  -b
 
 printf %"s\n" "" "* Creating Path to miniforge3 and activate it"
 source ${HOME}/miniforge3/bin/activate
-printf %"s\n" "" "* Creating Path to mamba and activate it"
-source ${HOME}/miniforge3/etc/profile.d/mamba.sh
-conda activate marigold
 
 printf %"s\n" "" "* Cleaning Up" ""
 rm "Miniforge3-"$(uname)-$(uname -m)".sh"
