c36930ae6694b81748b2fce7b65a7bf2dcb45c15
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-10 00:17:44 +0100

    Update 4_download_video_GDrive.sh

--- scripts/4_download_video_GDrive.sh
+++ scripts/4_download_video_GDrive.sh
@@ -10,11 +10,12 @@ if [ "$#" -gt 0 ]; then
 else
 	if [ ! -f "$VIDEO_ID_FILE" ]; then
 		read -e -p "Enter <video.mp4> Google Drive Link: " VIDEO_LINK
-		VIDEO_ID="$(echo "$VIDEO_LINK | awk -F "/" '{print $6}')"
+		VIDEO_ID="$(echo "$VIDEO_LINK" | awk -F "/" '{print $6}')"
 		touch $VIDEO_ID_FILE
 		echo $VIDEO_ID > $VIDEO_ID_FILE
 	else
 		VIDEO_ID=$(<$VIDEO_ID_FILE)
+		printf %"s\n"
 	fi
 	gdown --id $VIDEO_ID
 fi
