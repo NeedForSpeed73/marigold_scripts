93e622bdaf90b0a65b242696c89891df7dd7e220
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-10 00:02:13 +0100

    Update 3_clear_workspace.sh

--- scripts/3_clear_workspace.sh
+++ scripts/3_clear_workspace.sh
@@ -3,13 +3,11 @@
 DIR=$HOME/Marigold/workspace
 
 if [ -d "$DIR" ]; then
-    rm $DIR/input/*.png
-    rm -r $DIR/output/*
+    rm -r $DIR
     printf %"s\n" "Done."
 else
 	printf %"s\n" "$DIR  does not exist."
 	printf %"s\n" "* Creating $DIR."
-	mkdir $DIR/workplace
-	mkdir $DIR/workplace/input
-	mkdir $DIR/workplace/output
+	mkdir $DIR/input
+	mkdir $DIR/output
 fi
