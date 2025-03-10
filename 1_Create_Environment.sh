9703ec5af05ac8c1084c702cb1c95c070de68617
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-10 17:42:51 +0100

    Update 1_Create_Environment.sh

--- 1_Create_Environment.sh
+++ 1_Create_Environment.sh
@@ -12,19 +12,27 @@
 #	rm $DRIVERS_FILENAME
 #fi
 
+#Install CUDA Support
+printf %"s\n" "" "* Downloading and installing CUDA for Ubuntu 22.04 x86_64" ""
+IS_WSL="$(cat /proc/sys/fs/binfmt_misc/WSLInterop | grep enabled)"
+if [ $IS_WSL = "enabled" ]; then
+	wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+else	
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+fi
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-8
+
 printf %"s\n" "" "* Downloading Marigold (https://github.com/prs-eth/Marigold.git) and installing requirements-cuda" ""
 git clone https://github.com/prs-eth/Marigold.git
 cd Marigold
 
-CONDA_BASE=$(conda info --base)
-
 # Create Environment
 printf %"s\n" "" "* Creating Environment marigold" ""
-mamba env create -n marigold --file environment.yaml
-. $CONDA_BASE/etc/profile.d/conda.sh
-conda info --envs
-conda activate marigold
-conda info --envs #check if env is activated
+python -m venv venv/marigold
+source venv/marigold/bin/activate
+pip install -r requirements.txt
 
 printf %"s\n" "" "* Installing FFMPEG" ""
 sudo apt install -y ffmpeg
@@ -32,4 +40,3 @@ sudo apt install -y ffmpeg
 # Clean packagers
 printf %"s\n" "" "* Cleaning Up" ""
 sudo apt-get -y clean
-conda clean -y -a
