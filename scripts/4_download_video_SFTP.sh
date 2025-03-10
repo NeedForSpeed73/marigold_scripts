8056882e33d094dc2fb5a4d1a9102a491966d8c1
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-10 13:05:03 +0100

    Update 4_download_video_SFTP.sh

--- scripts/4_download_video_SFTP.sh
+++ scripts/4_download_video_SFTP.sh
@@ -6,5 +6,5 @@ DIR=$HOME/Marigold/workspace
 if [ "$#" -gt 1 ]; then
 	printf %"s\n" "Usage: 4_download_video_SFTP.sh. video file name in folder Public"
 else
-	sftp emilio@emiliofederici.ddns.net:/share/Public/$1 $DIR/video.mp4
+	sftp -P 24 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public/$1 $DIR/video.mp4
 fi
