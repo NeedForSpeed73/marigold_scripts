44ca81abfd5952b23c0aa65db404dce002087448
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 00:34:22 +0100

    Update 6_run_marigold.sh

--- 6_run_marigold.sh
+++ 6_run_marigold.sh
@@ -8,10 +8,10 @@ if [ "$#" -gt 1 ]; then
 else
 	if [ $1 = "--fast" ]; then
 		echo "Using LCM (faster) setting."
-		python run.py --input_rgb_dir $DIR/input --output_dir $DIR/output
+		python Marigold/run.py --input_rgb_dir $DIR/input --output_dir $DIR/output
 	else
 		echo "Using DDIM (paper) seting."
-		python run.py --checkpoint prs-eth/marigold-v1-0 --denoise_steps 50 --ensemble_size 10 --input_rgb_dir $DIR/input/ 
+		python Marigold/run.py --checkpoint prs-eth/marigold-v1-0 --denoise_steps 50 --ensemble_size 10 --input_rgb_dir $DIR/input/ 
     --output_dir $DIR/output	
 	fi
 fi
