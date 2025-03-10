fb059bca6c6052512c094aa718a860daf6259dca
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 00:32:24 +0100

    Update 5_video2images.sh

--- 5_video2images.sh
+++ 5_video2images.sh
@@ -3,7 +3,7 @@
 
 DIR=$HOME/Marigold/workspace
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -ne 1 ]; then
 	printf %"s\n" "Usage: 5_video2images.sh <video.mp4>"
 else
 	ffmpeg -i $DIR/$1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > $DIR/fps.txt
