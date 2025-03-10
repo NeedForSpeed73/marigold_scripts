333659bfc1880cd49ca96b5a8e70e96c01365f9f
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-10 13:30:31 +0100

    asd

--- scripts/5_video2images.sh
+++ scripts/5_video2images.sh
@@ -6,6 +6,6 @@ DIR=$HOME/Marigold/workspace
 if [ "$#" -gt 1 ]; then
 	printf %"s\n" "Usage: 5_video2images.sh <video.mp4>"
 else
-	ffmpeg -i $1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > fps.txt
-	ffmpeg -i $1 -qmin 1 -qscale:v 1 $DIR/input/%06d.jpg
+	ffmpeg -i $DIR/$1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > fps.txt
+	ffmpeg -i $DIR/$1 -qmin 1 -qscale:v 1 $DIR/input/%06d.jpg
 fi
--- scripts/6_run_marigold.sh
+++ scripts/6_run_marigold.sh
@@ -6,9 +6,12 @@ DIR=$HOME/Marigold/workspace
 if [ "$#" -gt 1 ]; then
 	printf %"s\n" "Usage: 6_run_marigold.sh <--fast>"
 else
-	if [ $1 == "--fast" ]
-		python run.py --input_rgb_dir $DIR/input --output_dir $DIR/output/
+	if [ $1 = "--fast" ]; then
+		echo "Using LCM (faster) setting."
+		python run.py --input_rgb_dir $DIR/input --output_dir $DIR/output
 	else
+		echo "Using DDIM (paper) seting."
 		python run.py --checkpoint prs-eth/marigold-v1-0 --denoise_steps 50 --ensemble_size 10 --input_rgb_dir $DIR/input/ 
     --output_dir $DIR/output	
+	fi
 fi
