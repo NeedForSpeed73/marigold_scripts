8df4659a11963fe5b8085a42b0b42389b542eae9
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 10:35:39 +0100

    Update 4_download_video_SFTP.sh

--- 4_download_video_SFTP.sh
+++ 4_download_video_SFTP.sh
@@ -6,5 +6,5 @@ DIR=$HOME/Marigold/workspace
 if [ "$#" -ne 1 ]; then
 	printf %"s\n" "Usage: 4_download_video_SFTP.sh. <video file> in folder Public"
 else
-	scp -P 24 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public/$1 $DIR/output/$1
+	scp -P 24 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public/$1 $DIR/input/$1
 fi
