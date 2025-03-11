1ef92b0777b5d997b56c4098a727b7d6bc2a03ea
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 11:03:16 +0100

    Update 7_images2video.sh

--- 7_images2video.sh
+++ 7_images2video.sh
@@ -16,9 +16,9 @@ else
 		filename=$($1 -- "$fullfile")
 		filename="${filename%.*}"
 		if [ -d "$DIR/output/averaged" ]; then
-			ffmpeg -framerate $FPS -i $DIR/output/averaged/%06d_pred.png -vcodec libx264 -pix_fmt yuv420p $DIR/output/$1_depth.mp4
+			ffmpeg -framerate $FPS -i $DIR/output/averaged/%06d_pred.png -vcodec libx264 -pix_fmt yuv420p $DIR/output/$filename_depth.mp4
 		else
-			ffmpeg -framerate $FPS -i $DIR/output/depth_bw/%06d_pred.png -vcodec libx264 -pix_fmt yuv420p $DIR/output/$1_depth.mp4
+			ffmpeg -framerate $FPS -i $DIR/output/depth_bw/%06d_pred.png -vcodec libx264 -pix_fmt yuv420p $DIR/output/$filename_depth.mp4
 		fi
 	else
 		printf %"s\n" "Error: File fps.txt not found."
