c9780788506a378fe81603c22cce7b2dd467a61a
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 11:21:34 +0100

    Update 8_upload_video_SFTP.sh

--- 8_upload_video_SFTP.sh
+++ 8_upload_video_SFTP.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
 
-DIR=$HOME/Marigold/workspace
-
 if [ "$#" -ne 1 ]; then
 	printf %"s\n" "Usage: 8_upload_video_SFTP.sh. <video file> in folder Public"
 else
-#	sftp -P 24 emilio@emiliofederici.ddns.net <<< put $out_file /share/CACHEDEV1_DATA/Public
-	scp -P 24 $DIR/output/$1 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public 
+	if ![ -f $1 ]; then
+		printf %"s\n" "File " $1 " not found."
+	else
+	#	sftp -P 24 emilio@emiliofederici.ddns.net <<< put $out_file /share/CACHEDEV1_DATA/Public
+		scp -P 24 $1 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public 
 fi
