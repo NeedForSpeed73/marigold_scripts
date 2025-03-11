588fd8d4d904979ddb1d561278361e8cbc700d72
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 11:25:53 +0100

    Update 8_upload_video_SFTP.sh

--- 8_upload_video_SFTP.sh
+++ 8_upload_video_SFTP.sh
@@ -8,5 +8,6 @@ else
 		printf %"s\n" "File " $1 " not found."
 	else
 	#	sftp -P 24 emilio@emiliofederici.ddns.net <<< put $out_file /share/CACHEDEV1_DATA/Public
-		scp -P 24 $1 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public 
+		scp -P 24 $1 emilio@emiliofederici.ddns.net:/share/CACHEDEV1_DATA/Public
+	fi
 fi
