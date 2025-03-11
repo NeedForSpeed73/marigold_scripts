08021ae691ae6ff165d9ded967bf943882553000
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-11 10:44:18 +0100

    Update 5_video2images.sh

--- 5_video2images.sh
+++ 5_video2images.sh
@@ -6,6 +6,10 @@ DIR=$HOME/Marigold/workspace
 if [ "$#" -ne 1 ]; then
 	printf %"s\n" "Usage: 5_video2images.sh <video.mp4>"
 else
-	ffmpeg -i $DIR/$1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > $DIR/fps.txt
-	ffmpeg -i $DIR/$1 -qmin 1 -qscale:v 1 $DIR/input/%06d.jpg
+	if ! [ -f $1 ]; then
+		printf %"s\n" "File $1 not found"
+	else
+		ffmpeg -i $1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p" > $DIR/fps.txt
+		ffmpeg -i $1 -qmin 1 -qscale:v 1 $DIR/input/%06d.jpg
+	fi
 fi
