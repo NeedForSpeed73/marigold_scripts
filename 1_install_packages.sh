6e103afc5ef6a27a1799440c54ad6442648bffd9
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-12 18:20:38 +0100

    Update 1_install_packages.sh

--- 1_install_packages.sh
+++ 1_install_packages.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #
 
+sudo apt update
+
 #Install CUDA Support for WSL
 IS_WSL="$(cat /proc/sys/fs/binfmt_misc/WSLInterop | grep enabled)"
 printf %"s\n" "" "* Downloading and installing CUDA for Ubuntu WSL" ""
 if [ $IS_WSL = "enabled" ]; then
 	wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+	sudo dpkg -i cuda-keyring_1.1-1_all.deb
+	sudo apt -y install cuda-toolkit-12-8
+	rm cuda-keyring_1.1-1_all.deb
 fi
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt update
-sudo apt -y install cuda-toolkit-12-8
-rm cuda-keyring_1.1-1_all.deb
 
 # Install ffmpeg
 printf %"s\n" "" "* Installing FFMPEG" ""
