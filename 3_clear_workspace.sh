b5c2f50fdc316dc2fe425c51fe94ce3a08121b87
NeedForSpeed73 <need_for_speed@libero.it>
2025-03-13 21:06:55 +0100

    Add files via upload

---
+++ 3_clear_workspace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$HOME/Marigold/workspace
+
+if [ -d "$DIR" ]; then
+    rm -r $DIR
+    printf %"s\n" "Done."
+else
+	printf %"s\n" "$DIR  does not exist."
+	printf %"s\n" "* Creating $DIR."
+	mkdir $DIR
+	mkdir $DIR/input
+	mkdir $DIR/output
+fi
+
+# Banner Message
+printf %"s\n" "" "***************************************************************"
+printf %"s\n" "* Now copy video file, gdrive account and swaks passwork files in Marigold"
+printf %"s\n" "***************************************************************" ""
